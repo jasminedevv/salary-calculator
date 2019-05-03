f4f3e7851fc136f329eb8578a7a4a77102dd2e29
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2019-05-03 13:29:36 -0700

    [fix] typo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Input, Header, Container, Divider } from 'semantic-ui-react'
+import { Input, Header, Container } from 'semantic-ui-react'
 
 function legible(num) {
   if (num == "") {
     return ""
   } else {
-  return parseInt(num)
+    return parseInt(num)
   }
 }
 
@@ -29,98 +28,96 @@ class App extends Component {
 
   calculateWeek(event) {
     let week = event.target.value
-    this.setState({week: week})
+    this.setState({ week: week })
 
-    let yearly = this.state.yearly
-    let monthly = this.state.monthly
     let hourly = this.state.hourly
 
     // with new hours/week, calculate salary
     let new_monthly = hourly * week * 4.3
-    this.setState({monthly: new_monthly})
+    this.setState({ monthly: new_monthly })
 
     let new_yearly = new_monthly * 12
-    this.setState({yearly: new_yearly})
+    this.setState({ yearly: new_yearly })
   }
 
   calculateHourly(event) {
     let hourly = event.target.value
-    this.setState({hourly: hourly})
+    this.setState({ hourly: hourly })
 
     let week = this.state.week
 
     // with new hourly value, calculate the other
     let new_monthly = hourly * week * 4.3
-    this.setState({monthly: new_monthly})
+    this.setState({ monthly: new_monthly })
 
     let new_yearly = new_monthly * 12
-    this.setState({yearly: new_yearly})
+    this.setState({ yearly: new_yearly })
   }
 
   calculateMonthly(event) {
     let monthly = event.target.value
-    this.setState({monthly: monthly})
+    this.setState({ monthly: monthly })
 
     let week = this.state.week
 
     // with new monthly value, calculate the others
     let new_yearly = monthly * 12
-    this.setState({yearly: new_yearly})
+    this.setState({ yearly: new_yearly })
 
     let new_hourly = monthly / 4.3 / week
-    this.setState({hourly: new_hourly})
+    this.setState({ hourly: new_hourly })
   }
 
   calculateYearly(event) {
     let yearly = event.target.value
-    this.setState({yearly: yearly})
+    this.setState({ yearly: yearly })
 
     let week = this.state.week
 
     // with new yearly value, set the new values
     let new_monthly = yearly / 12
-    this.setState({monthly: new_monthly})
+    this.setState({ monthly: new_monthly })
 
     let new_hourly = yearly / 52 / week // TODO: what's the equation?
-    this.setState({hourly: new_hourly})
+    this.setState({ hourly: new_hourly })
   }
 
   render() {
     return (
       <div className="App" >
-      <br/><br/>
-      <Header as='h1'>Salary Calculator</Header>
-      <br/><br/>
-      <Container className="max-300">
-      <Input fluid 
-        label="yearly" 
-        value={legible(this.state.yearly)}
-        onChange={this.calculateYearly}
-        />
-      <br/><br/>
-      <Input fluid 
-        label="monthly" 
-        value={legible(this.state.monthly)}
-        onChange={this.calculateMonthly}
-        />
-      <br/><br/>
-      <Input fluid 
-        label="hourly" 
-        value={legible(this.state.hourly)}
-        onChange={this.calculateHourly}
-        />
-      <br/><br/>
-      <Input fluid 
-        label="hours /week" 
-        value={legible(this.state.week)}
-        onChange={this.calculateWeek}
-        />
-      <br/><br/>
-      <p>Convert hourly pay to monthly and yearly based on hours in a workweek.</p>
-      <p>Numbers are rounded to the nearest integer and there are 4.3 weeks in a month.</p>
-      <br></br><br></br>
-      <a href="https://twitter.com/lacunahag">❤ a lacunahag project</a>
-      </Container>
+        <br /><br />
+        <Header as='h1'>Salary Calculator</Header>
+        <br /><br />
+        <Container className="max-300">
+          <Input fluid
+            label="yearly"
+            value={legible(this.state.yearly)}
+            onChange={this.calculateYearly}
+          />
+          <br /><br />
+          <Input fluid
+            label="monthly"
+            value={legible(this.state.monthly)}
+            onChange={this.calculateMonthly}
+          />
+          <br /><br />
+          <Input fluid
+            label="hourly"
+            value={legible(this.state.hourly)}
+            onChange={this.calculateHourly}
+          />
+          <br /><br />
+          <Input fluid
+            label="hours /week"
+            value={legible(this.state.week)}
+            onChange={this.calculateWeek}
+          />
+          <br /><br />
+          <p>Convert hourly pay to monthly and yearly based on hours in a workweek.</p>
+          <p>Numbers are rounded to the nearest integer and there are 4.3 weeks in a month.</p>
+          <br></br><br></br>
+          <a href="https://twitter.com/lacunahag">❤ a lacunahag project</a>
+        </Container>
       </div>
     );
   }
